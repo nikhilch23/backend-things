daefb6919862b6aefcd14aebff971c238bc55cc6
nikhilch23 <nik.ch.23@gmail.com>
2018-07-06 13:31:36 +0530

    Added a few basic file

---
+++ webserver.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+ 
+class WebServerHandler(BaseHTTPRequestHandler):  #Inheritance concept
+
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                message = ""
+                message += "<html><body>Hello!</body></html>"
+
+                self.wfile.write(bytes(message, 'utf-8'))    #Converts the string into a byte code (a requisite for python 3.x but not of 2.x)
+                print (message)
+                return
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), WebServerHandler)   #arguments are server address and a Request handler class
+        print ("Web Server running on port %s" % port)
+        server.serve_forever()     #Server will keep listening till an interuption occurs
+    except KeyboardInterrupt:     #Built in Keyboard interruption in python which is trigerred if the user enters ^C.
+        print (" ^C entered, stopping web server....")
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
