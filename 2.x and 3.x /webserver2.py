eef9604b5ad49015c6a65ae96c8b5f7913eb70e4
nikhilch23 <nik.ch.23@gmail.com>
2018-07-10 23:06:02 +0530

    Doubts regarding 2.x and 3.x

--- 2.x and 3.x /webserver2.py
+++ 2.x and 3.x /webserver2.py
@@ -1,4 +1,4 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
 
@@ -15,9 +15,8 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<h1>Hello!</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
-                self.wfile.write(bytes(output, 'utf-8'))
-                print (output)
-                
+                self.wfile.write(output)
+                print output
                 return
 
             if self.path.endswith("/hola"):
@@ -29,8 +28,8 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<h1>&#161 Hola !</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
-                self.wfile.write(bytes(output, 'utf-8'))
-                print (output)
+                self.wfile.write(output)
+                print output
                 return
 
         except IOError:
@@ -40,12 +39,9 @@ class webServerHandler(BaseHTTPRequestHandler):
         try:
             self.send_response(301)
             self.send_header('Content-type', 'text/html')
-            
             self.end_headers()
-
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            
-            print("vvvvvvvvvv")    
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 messagecontent = fields.get('message')
@@ -55,8 +51,8 @@ class webServerHandler(BaseHTTPRequestHandler):
             output += "<h1> %s </h1>" % messagecontent[0]
             output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
             output += "</body></html>"
-            self.wfile.write(bytes(output, 'utf-8'))
-            print (output)
+            self.wfile.write(output)
+            print output
         except:
             pass
 
@@ -65,10 +61,10 @@ def main():
     try:
         port = 8080
         server = HTTPServer(('', port), webServerHandler)
-        print ("Web Server running on port %s" % port)
+        print "Web Server running on port %s" % port
         server.serve_forever()
     except KeyboardInterrupt:
-        print (" ^C entered, stopping web server....")
+        print " ^C entered, stopping web server...."
         server.socket.close()
 
 if __name__ == '__main__':
