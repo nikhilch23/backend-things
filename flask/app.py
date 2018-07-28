3b0407a6d28e593e5950e3c8c43558b68ef675a5
nikhilch23 <nik.ch.23@gmail.com>
2018-07-28 23:41:30 +0530

    Modified app.py

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Restaurant, Base, MenuItem
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db',connect_args={'check_same_thread':False}) # Note: I have written connect_args because i was getting this error thread. It was saying the thread was created here and using somewhere else.
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -18,34 +18,40 @@ def display():
 	menus = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
 	
 
-@app.route('/restaurants/<int:restaurant_id>/')
-def rest(restaurant_id):
-	restaurant = session.query(Restaurant).filter_by(id = restaurant_id)
+@app.route('/restaurants/<int:restaurant_id>/', methods =['POST', 'GET'])
+def restaurantMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()   # Note: Writing one is crucial to signify you want only one restaurant
 	menus = session.query(MenuItem).filter_by (restaurant_id = restaurant_id)
-	output = ''
-	for menu in menus:
-		output += menu.name
-		output += '<br>'
-		output += menu.price
-		output += '<br>'
-		output += menu.description
-		output += '<br>'
-		output += '<a href='url_for('editMenuItem', restaurant_id = restaurant.id, menu_id = menu.id)'>Edit</a>'
+	return render_template('menu.html', restaurant = restaurant, menus = menus)
 
-		output += '<br>'
-	return output
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/new')
+@app.route('/restaurants/<int:restaurant_id>/new', methods =['POST', 'GET'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
-
+	if request.method == "POST":
+		newitem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+		session.add(newitem)
+		session.commit()
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit', methods =['POST', 'GET'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+	editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editedItem.name = request.form['name']
+		session.add(editedItem)
+		session.commit()
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		# USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+		return render_template(
+			'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
 # Task 3: Create a route for deleteMenuItem function here
 
@@ -56,7 +62,7 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 
 if __name__ == '__main__':
-
+	app.debug = True
 	app.run('localhost', 8080)
  
 
